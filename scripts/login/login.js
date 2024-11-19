0cc2483d63bc13103d0c8f4341b1db30aac5c046
gelasca <gelasca.dev@gmail.com>
2024-11-19 05:21:03 -0300

    feat⭐:some pages added

---
+++ scripts/login/login.js
@@ -0,0 +1,46 @@
+const  campoUsuario = document.querySelector('main form div input[type=text]')
+const  campoSenha = document.querySelector('main form div input[type=password]')
+
+var resultado = 0
+
+function logar(){
+    var usuario = campoUsuario.value
+    var senha = campoSenha.value
+    console.log(usuario,senha)
+    switch(usuario){
+        case 'aluno':{
+            resultado++
+            break
+        }
+        case 'professor':{
+            resultado--
+            break
+        }
+    }
+    switch(senha){
+        case 'aluno123':{
+            resultado++
+            break
+        }
+        case 'professor123':{
+            resultado--
+            break
+        }
+    }
+    switch (resultado) {
+        case 2:{
+            window.location.href = 'http\:\/\/127.0.0.1:5500\/home_discente.html'
+            break
+        }    
+        case -2:{
+            window.location.href = 'http\:\/\/127.0.0.1:5500\/home_docente.html'
+            break
+        }
+        default:{
+            alert("Usuário ou Senha não válidos.")
+            campoUsuario.value = ''
+            campoSenha.value = ''
+            break
+        }
+    }
+}
---
+++ scripts/questionario/questionario.js
@@ -0,0 +1,62 @@
+var perguntas = []
+var respostas = []
+perguntaAtual = 0
+
+function usaPerguntas(data){
+    console.log(data)
+}
+
+async function fetchData(){
+    try{
+        const response = await fetch('./scripts/questionario/questoes.json')
+        const data = await response.json()
+
+        usaPerguntas(data)
+        perguntas = data
+    }
+    catch(error){
+        console.error('Erro: '+error)
+    }
+}
+
+// window.onload = function(){    
+//     console.log('fdfs')
+//     // alert('h')
+// }
+
+function mudaPergunta(){
+    let alternativaMarcada
+    alternativas_elem.forEach((elem)=>{
+        if(elem.checked){
+            alternativaMarcada = Number(elem.value)
+        }
+    })
+    if(alternativaMarcada!=undefined){
+        respostas.push(alternativaMarcada)
+        perguntaAtual++
+        if(10>perguntaAtual){
+            console.log('aqui') 
+            pergunta_elem.innerHTML = perguntas.filter(elem=>elem.id == perguntaAtual+1)[0].questao
+            alternativas_elem.forEach((elem)=>{
+                elem.checked = false 
+            })
+        }
+        else{
+            // alert('acabou')
+            window.location.href = 'http://127.0.0.1:5500/pos_questionario.html'
+        }
+        if(perguntaAtual == 9){
+            botaoPergunta_elem.value = "Finalizar"
+            botaoPergunta_elem.style = "padding: 0.5em 1.5em;"+
+                "border-radius: 1.5em;"+
+                "font-size: 15pt;"
+        }
+    }
+}
+
+const form_group_elem = document.querySelector('main div.form-group')
+const pergunta_elem = form_group_elem.querySelector('h2')
+const alternativas_elem = form_group_elem.querySelectorAll('div div input[type=radio]')
+const botaoPergunta_elem = form_group_elem.querySelector('input[type=button]')
+
+fetchData()
